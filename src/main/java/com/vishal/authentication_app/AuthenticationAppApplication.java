cd0f682df100574ee1f9d2da229398898681bd6b
vishal patidar <136967663+vishal-patidar01@users.noreply.github.com>
2025-12-10 22:12:46 +0530

    Initial commit - authentication app

---
+++ src/main/java/com/vishal/authentication_app/AuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.authentication_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishal/authentication_app/AuthenticationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.authentication_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
