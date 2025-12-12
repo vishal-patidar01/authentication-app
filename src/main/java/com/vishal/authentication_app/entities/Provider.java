8f308f98cfab5847d8f9ed704fe150d7a5f102ab
vishal patidar <136967663+vishal-patidar01@users.noreply.github.com>
2025-12-12 22:28:17 +0530

    add a package entity

---
+++ src/main/java/com/vishal/authentication_app/entities/Provider.java
@@ -0,0 +1,5 @@
+package com.vishal.authentication_app.entities;
+
+public enum Provider {
+    LOCAL, GOOGLE, GITHUB, FACEBOOK
+}
---
+++ src/main/java/com/vishal/authentication_app/entities/Role.java
@@ -0,0 +1,24 @@
+package com.vishal.authentication_app.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    private UUID id=UUID.randomUUID();
+    @Column(unique = true, nullable = false)
+    private String name;
+}
