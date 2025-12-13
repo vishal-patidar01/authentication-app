8c771731087f1a07d52da491e66b24d75c5cc841
vishal patidar <136967663+vishal-patidar01@users.noreply.github.com>
2025-12-13 13:18:06 +0530

    Add new class UserDto

---
+++ src/main/java/com/vishal/authentication_app/dto/UserDto.java
@@ -0,0 +1,31 @@
+package com.vishal.authentication_app.dto;
+
+
+import com.vishal.authentication_app.entities.Provider;
+import com.vishal.authentication_app.entities.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+
+    private UUID id;
+    private  String email;
+    private String name;
+    private String password;
+    private String image;
+    private boolean enable = true;
+    private Instant createdAt = Instant.now();
+    private Instant updatedAt = Instant.now();
+    private Provider provider=Provider.LOCAL;
+    private Set<RoleDto> roles = new HashSet<>();
+}
