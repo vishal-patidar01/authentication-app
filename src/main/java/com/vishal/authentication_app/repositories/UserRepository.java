b8e1911681cd949cac38bee7cf017a60cd7a0f05
vishal patidar <136967663+vishal-patidar01@users.noreply.github.com>
2025-12-13 13:24:03 +0530

    add class

---
+++ src/main/java/com/vishal/authentication_app/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.vishal.authentication_app.repositories;
+
+import com.vishal.authentication_app.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean exitsByEmail(String email);
+}
